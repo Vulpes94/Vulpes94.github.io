327e60d6b3da4788dc0883a8cf5fbbba27e0c166
Vulpes94 <kjunfi94@gmail.com>
2023-11-17 17:45:03 +0900

    모델 디버깅 미완성

---
+++ QuoridorMini/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+document
+.querySelector(".piecePlayer")
+.addEventListener("click", function () {
+  alert("You clicked me!");
+});
+
+});
